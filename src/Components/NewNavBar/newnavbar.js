2cf782f1da2be0cdfeb0bc4f48e0ebfaa24db057
Shirley <shirley.huang2000@outlook.com>
2019-12-27 20:29:14 -0500

    "Added new nav bar template from reactstrap example"

---
+++ src/Components/NewNavBar/newnavbar.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText
+} from 'reactstrap';
+
+
+export class NewNavBar extends React.Component{
+
+    render(){
+        return (
+            <div className="newnavbar">
+                 <link
+                rel="stylesheet"
+                href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                crossorigin="anonymous"
+            />
+            
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand href="/">reactstrap</NavbarBrand>
+                    {/* <NavbarToggler onClick={toggle} />
+                    <Collapse isOpen={isOpen} navbar> */}
+                    <Nav className="mr-auto" navbar>
+                        <NavItem>
+                        <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            Options
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem>
+                            Option 1
+                            </DropdownItem>
+                            <DropdownItem>
+                            Option 2
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem>
+                            Reset
+                            </DropdownItem>
+                        </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <NavbarText>Simple Text</NavbarText>
+                    {/* </Collapse> */}
+                </Navbar>
+                </div>
+                </div>
+              
+        )
+    }
+}
+
+
+export default NewNavBar;
