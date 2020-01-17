b5e35427f61b4f93c6b197c742516a11a75a9dae
ShirleyofHuang <shirley.huang2000@outlook.com>
2020-01-17 11:16:01 -0500

    Updates

---
+++ precache-manifest.b622ac0e0330422fdc58c16f590840c8.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32a9b0ccfcb0979097c8a3f6875ae9ea",
+    "url": "/shirleyofhuang/index.html"
+  },
+  {
+    "revision": "51f84d356047dd85d052",
+    "url": "/shirleyofhuang/static/css/2.e8f3be53.chunk.css"
+  },
+  {
+    "revision": "606114a3877542b5ee3e",
+    "url": "/shirleyofhuang/static/css/main.9c9e1f5f.chunk.css"
+  },
+  {
+    "revision": "51f84d356047dd85d052",
+    "url": "/shirleyofhuang/static/js/2.9985e0e1.chunk.js"
+  },
+  {
+    "revision": "f509f139c0a29369d7b1c3800155cd22",
+    "url": "/shirleyofhuang/static/js/2.9985e0e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "606114a3877542b5ee3e",
+    "url": "/shirleyofhuang/static/js/main.a3f50409.chunk.js"
+  },
+  {
+    "revision": "054e07efe5d968737038",
+    "url": "/shirleyofhuang/static/js/runtime-main.6084a910.js"
+  },
+  {
+    "revision": "3441cf7dfd896b9065f18782352e654d",
+    "url": "/shirleyofhuang/static/media/Blog-Pink.3441cf7d.png"
+  },
+  {
+    "revision": "3a8fd2ce3ad9c98b06046f26fca0d896",
+    "url": "/shirleyofhuang/static/media/ThisIsMe.3a8fd2ce.png"
+  },
+  {
+    "revision": "74b77b656060aae123e4b931f12ff820",
+    "url": "/shirleyofhuang/static/media/calgary.74b77b65.jpg"
+  },
+  {
+    "revision": "d8b1db459cadcebb85358f60edea7ded",
+    "url": "/shirleyofhuang/static/media/collage.d8b1db45.jpg"
+  },
+  {
+    "revision": "6765587b4e01dbdc97f881f6414f9d5e",
+    "url": "/shirleyofhuang/static/media/greatwallofchina.6765587b.jpg"
+  },
+  {
+    "revision": "b460405995b5ccb78a6700cb69185b5b",
+    "url": "/shirleyofhuang/static/media/kyotojapan.b4604059.jpg"
+  },
+  {
+    "revision": "23446d575375a44c3c52a161bc1519aa",
+    "url": "/shirleyofhuang/static/media/laptopcafe.23446d57.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shirleyofhuang/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0c9ce7a812ac29e3d76c6d15dacb3901",
+    "url": "/shirleyofhuang/static/media/mepic.0c9ce7a8.jpg"
+  },
+  {
+    "revision": "cf99ca9eadb0e35a9b496fe2462aa189",
+    "url": "/shirleyofhuang/static/media/pic_me.cf99ca9e.jpg"
+  },
+  {
+    "revision": "614b6f03ec8809ce8ec56bf9bcaeeaae",
+    "url": "/shirleyofhuang/static/media/python.614b6f03.jpg"
+  },
+  {
+    "revision": "935c40275fa92071e0ee83421093621f",
+    "url": "/shirleyofhuang/static/media/pythontreemap.935c4027.png"
+  },
+  {
+    "revision": "97ba63723e656a712262d5b3959e8277",
+    "url": "/shirleyofhuang/static/media/shanghai.97ba6372.jpg"
+  },
+  {
+    "revision": "cf80eb58d4989531d04e56015337a00b",
+    "url": "/shirleyofhuang/static/media/simonsays.cf80eb58.jpg"
+  },
+  {
+    "revision": "71f45ea501dc24fccd04e655dfc8d688",
+    "url": "/shirleyofhuang/static/media/temp.71f45ea5.png"
+  },
+  {
+    "revision": "c07d82508d3df2657eed0c6163995228",
+    "url": "/shirleyofhuang/static/media/tianjinchina.c07d8250.jpg"
+  },
+  {
+    "revision": "30b1ef88891e30aa32ac21f8fa04fb09",
+    "url": "/shirleyofhuang/static/media/toronto.30b1ef88.jpg"
+  }
+]);
